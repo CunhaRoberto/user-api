e41dfb019f8a643be00ac8c981a0d901844ddc44
cunhaRoberto <rcunha@live.comm>
2025-07-12 12:45:55 -0300

    ajuste no use case para converter o param.email para LowerCase

--- src/authentication/use_cases/ForgotPassword.mjs
+++ src/authentication/use_cases/ForgotPassword.mjs
@@ -21,10 +21,10 @@ class CreateUsers {
       ENVIRONMENT 
     } = process.env
     // Check if the user exists
-    const user = await this.repository.getUserByEmail(param.email);
+    const user = await this.repository.getUserByEmail(param.email.toLowerCase());
 
     if (user.length < 1) {
-      throw new DataNotFoundException('User not found.');
+      throw new DataNotFoundException('User not found.');S
     }
 
     
